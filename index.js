7e92c2d1187bd717a70a2e8c40feca68c2f5159e
shrikantk3 <shri0402@yahoo.com>
2024-08-05 19:51:31 +0530

    simple deploy

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
-const conn = require('./www/config');
+// const conn = require('./www/config');
 // const _router = require('./routers/index');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json());
-conn.connect(err=>{
-    if(err){
-        throw err;
-    }else{
-        console.log('*********** DB Connection working !');
-    }
-})
-// app.use('/', _router);
+// conn.connect(err=>{
+//     if(err){
+//         throw err;
+//     }else{
+//         console.log('*********** DB Connection working !');
+//     }
+// })
+app.use('/', _router);
 
 
 app.listen(8080, err=>{
