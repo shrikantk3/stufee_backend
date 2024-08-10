b76f54021b2e4ccb213b08fe0400e74356ddab8a
shrikantk3 <shri0402@yahoo.com>
2024-08-10 14:30:11 +0530

    asas

--- www/config.js
+++ www/config.js
@@ -1,9 +1,9 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 conn = mysql.createConnection({
     host:'mysql-1534af81-abhash-1b57.g.aivencloud.com',
+    database:'defaultdb',
     user:'avnadmin',
     password:'AVNS_1neP3haGeRygTbF3C0g',
-    database:'defaultdb',
     port:24543
 });
 
